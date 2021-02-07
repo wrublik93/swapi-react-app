523ce1ebed597465e3feb04f99cc8a6aee516936
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-08 00:28:05 +0300

    feat: add catch error

--- swapi-react-app/src/index.js
+++ swapi-react-app/src/index.js
@@ -8,11 +8,17 @@
 
 const getResource = async(url) => {
   const res = await fetch(url);
+  if(!res.ok) {
+    throw new Error(`Your request ${url}, received status: ${res.status}`);
+  };
   const body = await res.json();
   return body;
 };
 
-getResource('https://swapi.dev/api/people/1/')
+getResource('https://swapi.dev/api/people/1sdd/')
   .then((body) => {
     console.log(body);
+  })
+  .catch((err) => {
+    console.error('Could not fetch: ', err);
   });
