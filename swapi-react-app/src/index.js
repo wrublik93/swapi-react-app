351bae663fb3f120bf934bb7e31562f6a53c03be
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-08 01:40:03 +0300

    feat: implement class resources

--- swapi-react-app/src/index.js
+++ swapi-react-app/src/index.js
@@ -6,7 +6,7 @@
     console.log(body);
   }) */
 
-const getResource = async(url) => {
+/* const getResource = async(url) => {
   const res = await fetch(url);
   if(!res.ok) {
     throw new Error(`Your request ${url}, received status: ${res.status}`);
@@ -21,4 +21,68 @@ getResource('https://swapi.dev/api/people/1sdd/')
   })
   .catch((err) => {
     console.error('Could not fetch: ', err);
+  }); */
+
+
+
+class SwapiService {
+
+  _apiBase = 'https://swapi.dev/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if(!res.ok) {
+      throw new Error(`Your request ${url}, received status: ${res.status}`);
+    };
+    return await res.json();
+  };
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    return res.results;
+  };
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}/`);
+  };
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    return res.results;
+  };
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}/`);
+  };
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    return res.results;
+  };
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}/`);
+  };
+};
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople()
+  .then((body) => {
+    console.log(body);
+  });
+
+  swapi.getAllPlanets()
+  .then((body) => {
+    console.log(body);
+  });
+
+  swapi.getPlanet(3)
+  .then((body) => {
+    console.log(body);
+  });
+
+  swapi.getAllStarships()
+  .then((body) => {
+    console.log(body);
   });
