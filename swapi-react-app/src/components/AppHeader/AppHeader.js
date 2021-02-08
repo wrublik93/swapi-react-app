df34f949f0b0f6f6f241abc956618a0f40e3c668
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-09 01:46:21 +0300

    feat: implement page layout App Header

---
+++ swapi-react-app/src/components/AppHeader/AppHeader.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './AppHeader.css';
+
+const AppHeader = () => {
+  return (
+    <div className="app-header d-flex">
+      <h3>
+        <a href="/">
+          Star DB
+        </a>
+      </h3>
+      <ul className="d-flex">
+        <li>
+          <a href="#">
+            People
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Planets
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Starships
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AppHeader;
