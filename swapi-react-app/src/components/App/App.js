ce667b6f54b1b80e460d48af58b01066fad1e1c2
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-09 01:45:55 +0300

    feat: implement page layout App

--- swapi-react-app/src/components/App/App.js
+++ swapi-react-app/src/components/App/App.js
@@ -1,3 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
+
+import AppHeader from '../AppHeader';
+import RandomPlanet from '../RandomPlanet';
+import ItemList from '../ItemList';
+import PersonDetails from '../PersonDetails';
 
 import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <RandomPlanet />
+      <div>
+        <div>
+          <ItemList />
+        </div>
+        <div>
+          <PersonDetails />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
