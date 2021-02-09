4a57e0108ed005937efae7438daebeaa2b5daf4a
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-09 23:48:01 +0300

    feat: implement page layout Person Details

---
+++ swapi-react-app/src/components/PersonDetails/PersonDetails.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import './PersonDetails.css';
+
+export default class PersonDetails extends Component {
+  render() {
+    return (
+      <div className="person-details card">
+        <img
+          alt="Person Face"
+          className="person-details-image"
+          src="https://starwars-visualguide.com/assets/img/characters/3.jpg"
+        />
+        <div className="card-body">
+        <h4>
+          Person Name
+        </h4>
+        <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">
+                Gender:
+              </span>
+              <span>
+                male
+              </span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">
+                Birth Year:
+              </span>
+              <span>
+                43
+              </span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">
+                Eye Color:
+              </span>
+              <span>
+                red
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
