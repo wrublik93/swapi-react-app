adbb4fddd9b8582b41e9e13b4207a5d563545704
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-11 16:21:13 +0300

    feat: implement page layout for Spinner

---
+++ swapi-react-app/src/components/Spinner/Spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-ball-xw1xq2m6hf">
+      <div className="ldio-tc65cmiadtg">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
